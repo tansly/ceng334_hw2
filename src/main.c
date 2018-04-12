6af0784d9d562936fe573b1af3eebb980944f095
Yagmur Oymak <yagmur.oymak@gmail.com>
2018-04-13 00:08:56 +0300

    Start, cancel and join ant threads

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "util.h"
 
+#include <assert.h>
 #include <curses.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -8,35 +9,82 @@
 #include <time.h>
 #include <unistd.h>
 
-static int n_ants;
-static int n_food;
-static int max_seconds;
+void *ant_main(void *arg)
+{
+    (void)arg;
+
+    for (;;) {
+        pthread_testcancel();
+    }
+
+    assert(0);
+    return NULL;
+}
 
 static void print_usage(char **argv)
 {
     fprintf(stderr, "Usage: %s n_ants n_food max_seconds\n", argv[0]);
 }
 
+static pthread_t *ants_create(int n_ants)
+{
+    int i;
+    pthread_t *threads = malloc(n_ants * sizeof *threads);
+    for (i = 0; i < n_ants; i++) {
+        if (pthread_create(&threads[i], NULL, ant_main, NULL) != 0) {
+            perror("ants_create(): pthread_create()");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return threads;
+}
+
+static void ants_cancel_join(pthread_t *threads, int n_ants)
+{
+    int i;
+    for (i = 0; i < n_ants; i++) {
+        if (pthread_cancel(threads[i]) != 0) {
+            perror("ants_cancel_join(): pthread_cancel()");
+            exit(EXIT_FAILURE);
+        }
+    }
+    for (i = 0; i < n_ants; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("ants_cancel_join(): pthread_join()");
+            exit(EXIT_FAILURE);
+        }
+    }
+    free(threads);
+}
+
 int main(int argc, char **argv)
 {
     srand(time(NULL));
 
+    int n_ants;
+    int n_food;
+    int max_seconds;
     if (argc != 4) {
         print_usage(argv);
         return 1;
     }
     if (sscanf(argv[1], "%d", &n_ants) != 1) {
         print_usage(argv);
-        return 1;
+        return EXIT_FAILURE;
     }
     if (sscanf(argv[2], "%d", &n_food) != 1) {
         print_usage(argv);
-        return 1;
+        return EXIT_FAILURE;
     }
     if (sscanf(argv[3], "%d", &max_seconds) != 1) {
         print_usage(argv);
-        return 1;
+        return EXIT_FAILURE;
     }
+
+    pthread_t *ant_threads = ants_create(n_ants);
+    ants_cancel_join(ant_threads, n_ants);
+
+    return 0;
 
     //////////////////////////////
     // Fills the grid randomly to have something to draw on screen.
@@ -91,14 +139,14 @@ int main(int argc, char **argv)
 
     // you have to have following command to initialize ncurses.
     startCurses();
-    
+
     // You can use following loop in your program. But pay attention to 
     // the function calls, they do not have any access control, you 
     // have to ensure that.
     char c;
     while (TRUE) {
         drawWindow();
-        
+
         c = 0;
         c = getch();
 
@@ -116,13 +164,13 @@ int main(int argc, char **argv)
             setSleeperN(getSleeperN()-1);
         }
         usleep(DRAWDELAY);
-        
+
         // each ant thread have to sleep with code similar to this
         //usleep(getDelay() * 1000 + (rand() % 5000));
     }
-    
+
     // do not forget freeing the resources you get
     endCurses();
-    
+
     return 0;
 }
