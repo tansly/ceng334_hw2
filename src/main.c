f5bd4b18a69890b8d6cda297795a7bbee52a005e
Yagmur Oymak <yagmur.oymak@gmail.com>
2018-04-12 20:14:21 +0300

    Obey the specs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include "do_not_submit.h"
+
+#include <curses.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
