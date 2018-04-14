252f41794763d06d5e1a7b1c52f0407c5dff035e
Yagmur Oymak <yagmur.oymak@gmail.com>
2018-04-14 14:24:01 +0300

    Semaphore implementation

---
+++ src/sem.h
@@ -0,0 +1,14 @@
+#ifndef SEM_H
+#define SEM_H
+
+struct semaphore;
+
+void semaphore_init(struct semaphore *sem, int value);
+
+void semaphore_wait(struct semaphore *sem);
+
+int semaphore_trywait(struct semaphore *sem);
+
+void semaphore_signal(struct semaphore *sem);
+
+#endif
